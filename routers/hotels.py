1009699f942e6950acddc1b3bb48092e30967b32
Dmitry <dodegov@smarthomecrafters.ai>
2024-11-27 19:31:15 +0300

    Перенесён и скорректирован список отелей

--- routers/hotels.py
+++ routers/hotels.py
@@ -1,9 +1,20 @@
 from fastapi import APIRouter, Query, Body
 
-from database import hotels
-from schemas.hotels import Hotel
+from schemas.hotels import Hotel, HotelPatch
 
 hotels_router = APIRouter(prefix="/hotels", tags=["Отели"])
+
+hotels = [
+    {"id": 1, "title": "Лондон", "name": "london"},
+    {"id": 2, "title": "Париж", "name": "paris"},
+    {"id": 3, "title": "Сочи", "name": "sochi"},
+    {"id": 4, "title": "Дубай", "name": "dubai"},
+    {"id": 5, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 6, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 7, "title": "Москва", "name": "moscow"},
+    {"id": 8, "title": "Казань", "name": "kazan"},
+    {"id": 9, "title": "Санкт-Петербург", "name": "spb"},
+]
 
 
 @hotels_router.get("/hotels")
@@ -11,6 +22,7 @@ def get_hotels(
         id: int | None = Query(None, description="ID отеля"),
         title: str | None = Query(None, description="Название отеля"),
 ):
+    global hotels
 
     hotels_ = []
     for hotel in hotels:
@@ -26,32 +38,35 @@ def get_hotels(
 def create_hotel(
         hotel_data: Hotel = Body(openapi_examples={
             "1": {
-                "summary": "Сочи",
+                "summary": "Madrid",
                 "value": {
-                    "title": "Отель Сочи 5 звезд у моря",
-                    "name": "sochi_u_morya",
+                    "title": "Мадрид",
+                    "name": "madrid",
                 }
             },
             "2": {
-                "summary": "Дубай",
+                "summary": "Barcelona",
                 "value": {
-                    "title": "Отель Дубай У фонтана",
-                    "name": "dubai_fountain",
+                    "title": "Барселона",
+                    "name": "barcelona",
                 }
             }
         })
 ):
+    global hotels
 
     hotels.append({
         "id": hotels[-1]["id"] + 1,
         "title": hotel_data.title,
         "name": hotel_data.name
     })
+
     return {"status": "OK"}
 
 
 @hotels_router.delete("/hotels/{hotel_id}")
 def delete_hotel(hotel_id: int):
+    global hotels
 
     hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
 
@@ -60,37 +75,34 @@ def delete_hotel(hotel_id: int):
 
 def update_name_and_or_title(hotel_id: int, new_name: str | None, new_title: str | None) -> bool:
 
+    global hotels
+
     idx, hotel = next(((index, hotel) for index, hotel in enumerate(hotels) if hotel['id'] == hotel_id), (None, None))
+
     if idx is not None:
         if new_name:
             hotel['name'] = new_name
         if new_title:
             hotel['title'] = new_title
         hotels[idx] = hotel
+
         return True
     else:
         return False
 
 
 @hotels_router.put("/hotels/{hotel_id}")
-def put_hotel(
-        hotel_id: int,
-        name: str = Body(),
-        title: str = Body(),
-):
+def put_hotel(hotel_id: int, data: Hotel):
     # Вызывается та же функция, что и для метода patch, т.к. внутри update_name_and_or_title
     # отрабатывается вариант отсутствия name или title, а т.к. в put в аргументах эти значения обязательны,
     # будут отработаны оба аргумента
 
-    result = update_name_and_or_title(hotel_id, name, title)
+    result = update_name_and_or_title(hotel_id, data.name, data.title)
     return {"status": "OK" if result else "Hotel not found"}
 
 
 @hotels_router.patch("/hotels/{hotel_id}")
-def patch_hotel(
-        hotel_id: int,
-        name: str | None = Body(None, description="name отеля"),
-        title: str | None = Body(None, description="title отеля"),
-):
-    result = update_name_and_or_title(hotel_id, name, title)
+def patch_hotel(hotel_id: int, data: HotelPatch):
+
+    result = update_name_and_or_title(hotel_id, data.name, data.title)
     return {"status": "OK" if result else "Hotel not found"}
