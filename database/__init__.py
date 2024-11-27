50d3db556ff0f05d74bf6c0a0fb5e9cef1986753
Dmitry <dodegov@smarthomecrafters.ai>
2024-11-27 17:00:39 +0300

    Список отелей вынесен отдельно

---
+++ database/__init__.py
@@ -0,0 +1,11 @@
+hotels = [
+    {"id": 1, "title": "London", "name": "london"},
+    {"id": 2, "title": "Paris", "name": "paris"},
+    {"id": 3, "title": "Sochi", "name": "sochi"},
+    {"id": 4, "title": "Дубай", "name": "dubai"},
+    {"id": 5, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 6, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 7, "title": "Москва", "name": "moscow"},
+    {"id": 8, "title": "Казань", "name": "kazan"},
+    {"id": 9, "title": "Санкт-Петербург", "name": "spb"},
+]
