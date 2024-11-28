148a491b3ac672f7f2073b01e4714a8d98a2aa33
Dmitry <dodegov@smarthomecrafters.ai>
2024-11-28 20:07:19 +0300

    Добавлено echo к настройке engine

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
-engine = create_async_engine(settings.db_url)
+engine = create_async_engine(settings.db_url, echo=False)
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
