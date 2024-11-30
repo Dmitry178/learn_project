2a05b8b862ef9d3133e09ff1711a8a76d5bb5142
Dmitry <dodegov@smarthomecrafters.ai>
2024-11-30 19:48:53 +0300

    Убран лишний код в модели таблицы users

--- src/models/users.py
+++ src/models/users.py
@@ -7,15 +7,8 @@ from src.database import Base
 class UsersOrm(Base):
 
     __tablename__ = "users"
-    __table_args__ = (
-        UniqueConstraint('lower_email', name='uq_lower_email'),
-    )
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
     email: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)  # можно использовать тип CITEXT
     hashed_password: Mapped[str] = mapped_column(String(200), nullable=False)
-
-    @property
-    def lower_email(self):
-        return self.email.lower()
