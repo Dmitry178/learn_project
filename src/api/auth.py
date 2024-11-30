8f646fd707072ab07e485d8437172193e95c3c92
Dmitry <dodegov@smarthomecrafters.ai>
2024-11-30 19:48:13 +0300

    Перенос кода, рефакторинг

---
+++ src/api/auth.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, HTTPException, Response
+from sqlalchemy.exc import IntegrityError
+
+from src.database import async_session_maker
+from src.repositories.users import UsersRepository
+from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
+
+auth_router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
+
+
+@auth_router.post("/login")
+async def login_user(response: Response, data: UserRequestAdd):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found")
+
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="User password incorrect")
+
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+
+        return {"access_token": access_token}
+
+
+@auth_router.post("/register")
+async def register_user(data: UserRequestAdd):
+    hashed_password = AuthService().hash_password(data.password)
+    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+
+    async with async_session_maker() as session:
+        try:
+            await UsersRepository(session).add(new_user_data)
+            await session.commit()
+
+        except IntegrityError:
+            return {"status": "User already exists"}
+
+        except Exception as ex:
+            print(ex)
+            print(type(ex))
+            return {"status": "Error creating user"}
+
+    return {"status": "OK"}
