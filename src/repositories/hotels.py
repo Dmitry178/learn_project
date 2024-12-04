ec9551892448fa817dbd9c713aa7630521714bac
Dmitry <dodegov@smarthomecrafters.ai>
2024-12-04 20:55:54 +0300

    Добавлен вариант решения из учебного репозитория (для истории)

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -75,3 +75,41 @@ class HotelsRepository(BaseRepository):
 
         result = await self.session.execute(query)
         return [self.schema.model_validate(model) for model in result.scalars().all()]
+
+    async def get_filtered_by_time(
+            self,
+            date_from: date,
+            date_to: date,
+            location,
+            title,
+            limit,
+            offset,
+    ) -> list[Hotel]:
+
+        # вариант из учебного репозитория
+
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
+
+        if location:
+            query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+
+        if title:
+            query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
+
+        query = (
+            query
+            .limit(limit)
+            .offset(offset)
+        )
+
+        result = await self.session.execute(query)
+
+        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
