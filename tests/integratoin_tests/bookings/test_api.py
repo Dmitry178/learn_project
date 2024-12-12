fccd8e14fdcef51f05fefdae52f605e144865d60
Dmitry <dodegov@smarthomecrafters.ai>
2024-12-12 11:43:49 +0300

    Добавлена проверка бронирования

--- tests/integratoin_tests/bookings/test_api.py
+++ tests/integratoin_tests/bookings/test_api.py
@@ -1,5 +1,8 @@
 import pytest
 
+from src.utils.db_manager import DBManager
+from tests.conftest import get_db_null_pool
+
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
     (1, "2024-08-01", "2024-08-10", 200),
@@ -29,3 +32,47 @@ async def test_add_booking(
         assert isinstance(result, dict)
         assert result["status"] == "OK"
         assert "data" in result
+
+
+@pytest.fixture(scope="module")
+async def db_m() -> DBManager:
+    async for db_m in get_db_null_pool():
+        yield db_m
+
+
+@pytest.fixture(scope="module")
+async def delete_bookings(db_m):
+    await db_m.bookings.delete()
+    await db_m.commit()
+    yield
+    pass
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code, num_bookings", [
+    (1, "2024-08-01", "2024-08-10", 200, 1),
+    (1, "2024-08-02", "2024-08-11", 200, 2),
+    (1, "2024-08-03", "2024-08-12", 200, 3),
+    (1, "2024-08-04", "2024-08-13", 200, 4),
+    (1, "2024-08-05", "2024-08-14", 200, 5),
+    (1, "2024-08-06", "2024-08-15", 400, 5),
+    (1, "2024-08-17", "2024-08-25", 200, 6),
+])
+async def test_add_and_get_bookings(
+        room_id, date_from, date_to, status_code, num_bookings,
+        delete_bookings, aac
+):
+    response_bookings = await aac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response_bookings.status_code == status_code
+
+    response_me = await aac.get("/bookings/me")
+    assert response_me.status_code == 200
+    json_data = response_me.json()
+    assert isinstance(json_data, list)
+    assert len(response_me.json()) == num_bookings
